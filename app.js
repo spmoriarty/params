938a530702205e197f8bb7d4dee23b2e8ebd6900
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-09 11:42:48 -0700

    setting up new HTML/JS files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { animals } from './data.js';
 import { renderListItem } from './render-utils.js';
+import { findById } from './utils.js';
 
 const main = document.querySelector('main');
 
@@ -7,3 +8,8 @@ for (let animal of animals) {
     const animalDiv = renderListItem(animal);
     main.append(animalDiv);
 }
+
+
+
+
+for (let )
--- render-utils.js
+++ render-utils.js
@@ -11,6 +11,15 @@ export function renderListItem(animal) {
     nameSpan.textContent = animal.name;
     nameSpan.classList.add('name');
 
+    const li = document.createElement('li')
+    const a = document.createElement('a'),
+
+    a.href = `./data/?id=${animals.id}`,
+    a.textContent = animals.name,
+    li.append(a),
+    return li;
+
+
     const span = document.createElement('span');
     span.textContent = animal.says;
 
